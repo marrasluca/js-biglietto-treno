32f6b65e02acc07a17d74ea9300337686b2b1254
marrasluca <lmarras10@gmail.com>
2025-01-11 17:21:05 +0100

    add: fase di preparazione

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-/*
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-- il prezzo del biglietto è definito in base ai km (0.21 € al km)
-- va applicato uno sconto del 20% per i minorenni
-- va applicato uno sconto del 40% per gli over 65.
-- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
-  Questo richiederà un minimo di ricerca
-/*
+//fase di preparazione
+const prezzoBigliettoKm = 0.21
+let km = prompt("Qunati chilometri devo percorrere con il treno?")
+let eta = parseInt( prompt("Quanti anni hai?") )
+let scontoMinorenni = 0.20
+let scontoOver = 0.40
+let prezzoBiglietto
+
+//fase di elaborazione
+
+//fase di output
