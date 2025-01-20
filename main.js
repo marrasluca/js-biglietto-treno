500febb9179384d3c2f5d7174d57d706189006eb
marrasluca <lmarras10@gmail.com>
2025-01-20 12:11:55 +0100

    end

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 //fase di preparazione
 const prezzoBigliettoKm = 0.21
-let km = prompt("Qunati chilometri devo percorrere con il treno?")
+let km = prompt("Quanti chilometri devo percorrere con il treno?")
 let eta = parseInt( prompt("Quanti anni hai?") )
 let scontoMinorenni = 0.20
 let scontoOver = 0.40
 let prezzoBiglietto
 
 //fase di elaborazione
-if( eta > 18 ){
+if( eta < 18 ){
     prezzoBiglietto = (prezzoBigliettoKm * km) * scontoMinorenni
 }
 if ( eta > 65) {
